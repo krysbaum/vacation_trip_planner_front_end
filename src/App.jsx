c9b6117c7396c9abd32d7c74930b8ebeb7fcf239
Krys <krysbaum@gmail.com>
2024-06-06 18:08:12 -0700

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { BrowserRouter } from "react-router-dom";
-import { axios } from "axios";
+import axios from "axios";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://vacation-trip-planner-api.onrender.com";
